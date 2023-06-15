c579659f5a0b825a845e2ad9eecd05179aab5d3f
sciszkie <stanislaw.ciszkeiwicz@pw.edu.pl>
2023-06-15 22:25:53 +0200

    background+buckets

--- src/main/java/GUI/CalicoApplication.java
+++ src/main/java/GUI/CalicoApplication.java
@@ -20,7 +20,7 @@ public class CalicoApplication extends Application {
         Scene scene = new Scene (root);
 
         stage.setScene(scene);
-        stage.setResizable(false);
+        //stage.setResizable(false);
         stage.show();
     }
 
