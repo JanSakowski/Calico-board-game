744c6c6d6a2673ef499fce79130f3ca4bcae50b5
Michał Kozłowski <01178281@pw.edu.pl>
2023-06-16 09:17:25 +0200

    apllication and singleton classes javadoc; confirmation dialog window on closing attempt

--- src/main/java/GUI/CalicoApplication.java
+++ src/main/java/GUI/CalicoApplication.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
@@ -12,11 +14,34 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 
+/**
+ * Application class, that opens welcome screen scene
+ */
 public class CalicoApplication extends Application {
-
+    /**
+     * Overridden start method that adjusts basic properties of the game window
+     * @param stage the primary stage for this application, onto which
+     * the application scene can be set.
+     * Applications may create other stages, if needed, but they will not be
+     * primary stages.
+     * @throws IOException input/output exception
+     */
     @Override
     public void start(Stage stage) throws IOException {
         stage.setTitle("Calico");
+        stage.setOnCloseRequest(windowEvent -> {
+            windowEvent.consume();
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation");
+            alert.setHeaderText("Are you sure you want to close the window?");
+            alert.setContentText("Any unsaved changes will be lost.");
+
+            ButtonType result = alert.showAndWait().orElse(ButtonType.CANCEL);
+
+            if (result == ButtonType.OK) {
+                stage.close();
+            }
+        });
         FXMLLoader loader = new FXMLLoader(getClass().getResource("welcomescreen.fxml"));
         URL url = getClass().getResource("/GUI/cats/CiraButton.png");
         Image icon = new Image(url.toString());
@@ -29,7 +54,10 @@ public class CalicoApplication extends Application {
         stage.show();
     }
 
-
+    /**
+     * Static main method
+     * @param args args
+     */
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/GUI/GameDataSingleton.java
+++ src/main/java/GUI/GameDataSingleton.java
@@ -2,15 +2,35 @@ package GUI;
 
 import gamepackage.Game;
 
+/**
+ * Singleton type class responsible for storing date, that is passed
+ * between two scenes
+ */
 public class GameDataSingleton {
+    /**
+     * Static instance of singleton class.
+     */
     private static final GameDataSingleton instance = new GameDataSingleton();
     public static GameDataSingleton getInstance() {
         return instance;
     }
+
+    /**
+     * Stores information about number of players
+     */
     private static int numberOfPlayers;
+    /**
+     * Stores information about current game
+     */
     private static Game game;
+    /**
+     * Stores information about players' names
+     */
     private static String[] playerNames = new String[4];
 
+    /**
+     * Default constructor
+     */
     private GameDataSingleton() {}
     public void setPlayerNames(String[] table) {
         for (int i = 0; i < numberOfPlayers; i++) {
